76e35db50cbed7a19800ef6cf5cf7dfc4b6b80be
Nicola Racco <nicola@nicolaracco.com>
2015-03-17 14:34:11 +0100

    url composed correctly

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-	"strconv"
+	"net/url"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/julienschmidt/httprouter"
 )
+
+func composeStaticDataAPI(params httprouter.Params, values url.Values) string {
+	baseURL, err := url.Parse(
+		fmt.Sprintf(
+			"https://global.api.pvp.net/api/lol/static-data/%s/v1.2/%s/%s",
+			params.ByName("region"), params.ByName("thing"), params.ByName("id"),
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	values.Set("api_key", os.Getenv("RIOT_API_KEY"))
+	baseURL.RawQuery = values.Encode()
+	return baseURL.String()
+}
 
 func staticDataAction(c *gin.Context) {
 	c.Request.ParseForm()
-	count := strconv.Itoa(len(c.Params))
-	count2 := strconv.Itoa(len(c.Request.Form))
-	c.String(http.StatusOK, "pong-"+count+"-"+count2)
+
+	baseURL := composeStaticDataAPI(c.Params, c.Request.Form)
+	c.String(http.StatusOK, baseURL)
 }
 
 func main() {
@@ -19,8 +38,8 @@ func main() {
 
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/:thing", staticDataAction)
-		v1.GET("/:thing/:id", staticDataAction)
+		v1.GET("/:region/:thing", staticDataAction)
+		v1.GET("/:region/:thing/:id", staticDataAction)
 	}
 
 	r.Run(":8080")
